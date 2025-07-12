cbe9844afef13c4cde3cea06e20d47f5bf367843
Alan Silva <alanclash2541@gmail.com>
2025-07-11 21:10:41 -0300

    Add initial Go module and server implementation

---
+++ server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var startedAt = time.Now()
+
+func main() {
+	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)
+	fmt.Println("Servidor rodando em http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s and I'm %s years old", name, age)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s, Password: %s", user, password)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My family: %s.", string(data))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "Duration: %v", duration.Seconds())
+		return
+	}
+
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "ok")
+}
